ed0513832751b2355733772eb8d05b01011e7dd3
jedi_1993 <henri.oliveira.93@gmail.com>
2024-02-26 00:20:44 -0300

    subindo projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const DataFake = [
+    {
+        "id":"1",
+        "title":"Novo homem de ferro em 3d",
+        "description":"ola",
+        "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+    },
+    {
+        "id":"2",
+        "title":"Nova serie da marvel",
+        "description":"nova serie adicionada na disney",
+        "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+
+    }
+]
